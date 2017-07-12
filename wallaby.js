2fbee7859ff53e147a2560e39887ec4dce77b114
Artem Govorov <artem.govorov@gmail.com>
2017-07-12 22:08:12 +1000

    Update wallaby.js

--- wallaby.js
+++ wallaby.js
@@ -1,10 +1,18 @@
 /* eslint-disable */
 const wallabyWebpack = require('wallaby-webpack');
 const webpackConfig = require('./build/webpack.test.conf');
-const webpackPostProcessor = wallabyWebpack(webpackConfig);
+
 module.exports = function (wallaby) {
+  
+  webpackConfig.resolve.alias['@'] = require('path').join(wallaby.projectCacheDir, 'src');
+  const webpackPostProcessor = wallabyWebpack(webpackConfig);
+  
 	return {
 		files: [
+      {
+        pattern: '.babelrc',
+        instrument: false
+      },
 			{
 				pattern: 'node_modules/babel-polyfill/dist/polyfill.js',
 				instrument: false
